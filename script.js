c32eebe9c3c6564d787e221eae5059dfe2cd74d9
ENG AYUUB <moztaphem9@gmal.com>
2023-06-15 18:57:10 +0300

    Find7 DONE

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Good Luck 💪🏾
+
+
+let findSeven = number => {
+    for (let i = 0; i < number.length; i++) {
+      if (number[i] === 7) {
+        return "FOUND";
+      }
+    }
+    return "NOT FOUND";
+  }
+  
+  console.log(findSeven([1, 2, 3, 4, 5, 6,7]));
